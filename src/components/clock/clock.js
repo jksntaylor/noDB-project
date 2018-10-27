4e9ff04307b534180ef692b5fb0dc0ed9b6b58ed
Jackson Taylor <josephjacksontaylor@gmail.com>
2018-10-26 21:03:55 -0600

    added month name to clock

--- src/components/clock/clock.js
+++ src/components/clock/clock.js
@@ -8,10 +8,12 @@ class Clock extends Component {
         super ()
         this.state = {
             hour: 0,
+            amPm: 'AM',
             minute: 0,
             month: 0,
-            date: 0
-
+            date: 0,
+            year: 0,
+            monthName: ''
         }
         this.getTime = this.getTime.bind(this);
     }
@@ -19,12 +21,27 @@ class Clock extends Component {
     getTime() {
        axios.get('http://worldclockapi.com/api/json/mst/now').then(res => {
             let { currentDateTime } = res.data;
-            var date = new Date(currentDateTime)
+            var dateObj = new Date(currentDateTime)
+            var hour = dateObj.getHours();
+            if (hour>12) {
+                hour -=12;
+                this.setState({amPm: 'PM'})
+            } else if (hour===0) {
+                hour = 12;
+                this.setState({amPm: 'PM'})
+            };
+            var minute = dateObj.getMinutes();
+            var month = dateObj.getMonth();
+            let monthArr = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']
+            var monthName = monthArr[month];
+            var day = dateObj.getDay();
+            var year = dateObj.getFullYear();
             this.setState({
-                hour: date.getHours(),
-                minute: date.getMinutes(),
-                month: date.getMonth()+1,
-                date: date.getDate(),
+                hour: hour,
+                minute: minute,
+                month: monthName,
+                date: day,
+                year: year
             })
            })
            
@@ -38,10 +55,16 @@ class Clock extends Component {
     render() {
         return (
             <div>
-                <h1>{this.state.hour}</h1>
-                <h1>{this.state.minute}</h1>
-                <h1>{this.state.month}</h1>
-                <h1>{this.state.date}</h1>
+                <div className="time">
+                    <h1 className="hour">{this.state.hour}</h1>
+                    <h1 className="minute">{this.state.minute}</h1>
+                    <h1 className="amPm">{this.state.amPm}</h1>
+                </div>
+                <div className="date">
+                    <h1 className="month">{this.state.month}</h1>
+                    <h1 className="day">{this.state.date}</h1>
+                    <h1 className="year">{this.state.year}</h1>
+                </div>
             </div>
         )
     }
